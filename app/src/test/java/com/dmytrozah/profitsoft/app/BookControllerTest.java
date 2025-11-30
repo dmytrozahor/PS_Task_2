00deef36efaf6544936316f1517f539488cb86c3
Dmytro Zahorodnii <dmytrozah.dev@gmail.com>
2025-11-30 23:33:22 +0100

    BookControllerTest#testDeleteBook, etc.
    Signed-off-by: Dmytro Zahorodnii <dmytrozah.dev@gmail.com>

--- app/src/test/java/com/dmytrozah/profitsoft/app/BookControllerTest.java
+++ app/src/test/java/com/dmytrozah/profitsoft/app/BookControllerTest.java
@@ -4,6 +4,7 @@ import com.dmytrozah.profitsoft.Task2App;
 import com.dmytrozah.profitsoft.domain.entity.BookData;
 import com.dmytrozah.profitsoft.domain.repository.BookAuthorRepository;
 import com.dmytrozah.profitsoft.domain.repository.BookRepository;
+import com.dmytrozah.profitsoft.rest.dto.BookListDto;
 import com.dmytrozah.profitsoft.rest.dto.RestResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,16 +12,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(
@@ -43,38 +47,37 @@ class BookControllerTest {
 
     private String name = "Zahorodnii Dmytro";
 
-    private String bookName = "My little story";
-    private String genres = "Romantic, Tragedy";
+    private final String bookName = "My little story";
+    private final String genres = "Romantic, Tragedy";
 
-    String bookSaveBody = """
-                {
-                    "title": "%s",
-                    "author": "%s",
-                    "genres": "%s"
-                }
-                """.formatted(bookName, name, genres);
+    private final String bookSaveBody = """
+            {
+                "title": "%s",
+                "author": "%s",
+                "genres": "%s"
+            }
+            """.formatted(bookName, name, genres);
 
-    private String authorSaveBody = """
-                {
-                    "name": "%s"
-                }
-                """.formatted(name);
+    private final String authorSaveBody = """
+            {
+                "name": "%s"
+            }
+            """.formatted(name);
 
+    private final String newTitle = "My big story";
+    private final String newGenres = "A, B, C";
 
-    private String newTitle = "My big story";
-    private String newGenres = "A, B, C";
-
-    private String updateBody = """
-                {
-                    "title": "%s",
-                    "author": "%s",
-                    "genres": "%s"
-                }
-                """.formatted(newTitle, name, newGenres);
+    private final String updateBody = """
+            {
+                "title": "%s",
+                "author": "%s",
+                "genres": "%s"
+            }
+            """.formatted(newTitle, name, newGenres);
 
 
     @BeforeEach
-    void populateAuthors() throws Exception {
+    void populateAuthor() throws Exception {
         mockMvc.perform(post("/api/authors")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(authorSaveBody)
@@ -90,8 +93,8 @@ class BookControllerTest {
     @Test
     public void testCreateBook() throws Exception {
         MvcResult result = mockMvc.perform(post("/api/books")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(bookSaveBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bookSaveBody)
         ).andExpect(status().isCreated()).andReturn();
 
         RestResponse response = parseResponse(result, RestResponse.class);
@@ -110,9 +113,9 @@ class BookControllerTest {
     @Test
     public void testCreateBookValidation() throws Exception {
         mockMvc.perform(post("/api/books")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{}"))
-        .andExpect(status().isBadRequest());
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
@@ -126,7 +129,7 @@ class BookControllerTest {
 
         long bookId = Long.parseLong(response.getMessage());
 
-        MvcResult updateRes = mockMvc.perform(put("/api/books/" + bookId)
+        mockMvc.perform(put("/api/books/" + bookId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(updateBody)
         ).andExpect(status().isOk()).andReturn();
@@ -138,7 +141,168 @@ class BookControllerTest {
         assertThat(data.getGenres()).isEqualTo(newGenres);
     }
 
-    private <T> T parseResponse(MvcResult result, Class<T> c){
+    @Test
+    public void testListBooks() throws Exception {
+        List<Long> ids = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            String itBookSaveBody = """
+            {
+                "title": "%s",
+                "author": "%s",
+                "genres": "%s"
+            }
+            """.formatted(bookName + i, name, genres);
+
+            MvcResult result = mockMvc.perform(post("/api/books")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(itBookSaveBody)
+            ).andExpect(status().isCreated()).andReturn();
+
+            RestResponse response = parseResponse(result, RestResponse.class);
+
+            ids.add(Long.parseLong(response.getMessage()));
+        }
+
+        MvcResult result = mockMvc.perform(post("/api/books/_list")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "size": 50,
+                            "page": 0
+                        }
+                        """)
+        ).andExpect(status().isOk()).andReturn();
+
+        BookListDto listDto = parseResponse(result, BookListDto.class);
+
+        assertThat(listDto).isNotNull();
+        assertThat(listDto.getInfos().size()).isEqualTo(ids.size());
+
+        for (int i = 0; i < listDto.getInfos().size(); i++) {
+            final long id = listDto.getInfos().get(i).getId();
+
+            assertThat(ids.contains(id)).isTrue();
+        }
+    }
+
+    @Test
+    public void testDeleteBook() throws Exception {
+        MvcResult saveRes = mockMvc.perform(post("/api/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bookSaveBody)
+        ).andExpect(status().isCreated()).andReturn();
+
+        RestResponse response = parseResponse(saveRes, RestResponse.class);
+
+        long bookId = Long.parseLong(response.getMessage());
+
+        mockMvc.perform(delete("/api/books/" + bookId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updateBody)
+        ).andExpect(status().isOk()).andReturn();
+
+        BookData data = bookRepository.findById(bookId).orElse(null);
+
+        assertThat(data).isNull();
+    }
+
+    @Test
+    public void testGenerateReport() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            String itBookSaveBody = """
+            {
+                "title": "%s",
+                "author": "%s",
+                "genres": "%s"
+            }
+            """.formatted(bookName + i, name, genres);
+
+            mockMvc.perform(post("/api/books")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(itBookSaveBody)
+            ).andExpect(status().isCreated()).andReturn();
+        }
+
+        MvcResult reportRes = mockMvc.perform(post("/api/books/_report")
+        ).andExpect(status().isOk())
+                .andExpect(header().string(
+                        HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE)
+                )
+                .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=books.csv"))
+                .andReturn();
+
+        final String content = reportRes.getResponse().getContentAsString();
+
+        assertThat(content).isNotNull();
+        assertThat(content).startsWith("Title,Author\n");
+
+        for (int i = 0; i < 10; i++){
+            assertThat(content).contains(bookName + i);
+        }
+    }
+
+    @Test
+    public void testGenerateReportAuthor() throws Exception {
+        String diffAuthor = name + "-diff";
+
+        String authorSaveBody = """
+            {
+                "name": "%s"
+            }
+            """.formatted(diffAuthor);
+
+        final RestResponse creationRes = parseResponse(
+                mockMvc.perform(post("/api/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(authorSaveBody)
+                ).andExpect(status().isCreated())
+                        .andReturn(), RestResponse.class);
+
+        final long newAuthorId = Long.parseLong(creationRes.getMessage());
+
+        for (int i = 0; i < 10; i++) {
+            String itBookSaveBody = """
+            {
+                "title": "%s",
+                "author": "%s",
+                "genres": "%s"
+            }
+            """.formatted(bookName + i, i == 9 ? diffAuthor : name, genres);
+
+            mockMvc.perform(post("/api/books")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(itBookSaveBody)
+            ).andExpect(status().isCreated()).andReturn();
+        }
+
+        String generateReportAuthor = """
+                {
+                    "author_id": %d
+                }
+                """;
+
+        MvcResult reportRes = mockMvc.perform(post("/api/books/_report")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(generateReportAuthor.formatted(newAuthorId))
+                ).andExpect(status().isOk())
+                .andExpect(header().string(
+                        HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE)
+                )
+                .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=books.csv"))
+                .andReturn();
+
+        final String content = reportRes.getResponse().getContentAsString();
+
+        assertThat(content).isNotNull();
+        assertThat(content).startsWith("Title,Author\n");
+
+        for (int i = 0; i < 9; i++) {
+            assertThat(content).doesNotContain(bookName + i);
+        }
+    }
+
+    private <T> T parseResponse(MvcResult result, Class<T> c) {
         try {
             return mapper.readValue(result.getResponse().getContentAsString(), c);
         } catch (UnsupportedEncodingException e) {
